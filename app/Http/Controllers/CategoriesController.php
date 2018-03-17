9a06b7a0258211ec1401e2a121c418489d721525
blithe404 <blithe404@gmail.com>
2018-03-18 01:28:22 +0800

    话题列表排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -7,8 +7,11 @@ use App\Models\Topic;
 use App\Models\Category;
 
 class CategoriesController extends Controller {
-    public function show(Category $category) {
-        $topics = Topic::where('category_id', $category->id)->paginate(20);
+    public function show(Category $category, Request $request, Topic $topic) {
+
+        $topics = $topic->withOrder($request->order)
+            ->where('category_id', $category->id)
+            ->paginate(20);
         return view('topics.index', compact('topics', 'category'));
     }
 }
